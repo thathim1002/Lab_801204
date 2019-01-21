aae67cd28b2957c63b27dd1114cdd07ebd9f5032
thathim1002 <45302599+thathim1002@users.noreply.github.com>
2019-01-21 13:57:33 +0700

    init project

---
+++ BasicLayout/app/src/main/java/basiclayout/thathim/th/ac/su/ict/basiclayout/MainActivity.java
@@ -0,0 +1,13 @@
+package basiclayout.thathim.th.ac.su.ict.basiclayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
